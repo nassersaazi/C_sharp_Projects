f977ce76d2ec7898dcd2555ae29d6937bce6c340
nassersaazi <nassersnoop@gmail.com>
2019-11-13 10:29:45 +0300

    Storing data in text files

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -10,12 +10,7 @@ namespace TrackerLibrary.DataAccess
     {   // TODO - Wire up the CreatePrize for text files 
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            // Load the text file
-            // Convert the text to List<PrizeModel>
-            // Find max ID
-            //Add the new record with the new ID (max + 1)
-            // Convert the prizes to list<string>
-            // Save the list<string> to the text file
+            
         }
     }
 }
--- TournamentTracker/TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+//using System.Configuration;
 using System.Collections.Generic;
 using System.Text;
+
+// Load the text file
+// Convert the text to List<PrizeModel>
+// Find max ID
+//Add the new record with the new ID (max + 1)
+// Convert the prizes to list<string>
+// Save the list<string> to the text file
 
 namespace TrackerLibrary.DataAccess.TextConnector
 {
     public static class TextConnectorProcessor
     {
+        public static string FullFilePath(string fileName ) //PrizeModels.csv
+        {
+            return $"{ ConfigurationManager.AppSettings["filePath"] }\\{ fileName}"
+        }
     }
 }
