976be79d050f5ee9758cf953b565e4187e233145
nassersaazi <nassersnoop@gmail.com>
2019-12-10 05:16:10 +0300

    Windows background service
    A service that logs the time every second

---
+++ LoggingService/LoggingService/Logging.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;            
+using System.Timers;
+
+namespace LoggingService
+{
+    public class Logging
+    {
+        private readonly Timer _timer;
+
+        public Logging()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+            
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Demos\Logs.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ LoggingService/LoggingService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace LoggingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Logging>(s =>
+                {
+                    s.ConstructUsing(logging => new Logging());
+                    s.WhenStarted(logging => logging.Start());
+                    s.WhenStopped(logging => logging.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("LoggingService");
+                x.SetDisplayName("Logging Service");
+                x.SetDescription("This is the sample service that logs events every second.");
+
+               
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
