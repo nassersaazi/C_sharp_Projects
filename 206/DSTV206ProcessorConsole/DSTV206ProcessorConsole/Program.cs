238c6473e372fd4cd18cf92c90d071a45d0310a6
nassersaazi <nassersnoop@gmail.com>
2021-02-23 14:27:44 +0300

    Initial 206 test code

---
+++ 206/DSTV206ProcessorConsole/DSTV206ProcessorConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSTV206ProcessorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pick txns from mobile money received where sendToVendor = 1 and Status is SUCCESS
+            // Insert the txns in bill payments with Status INSERTED
+            // If insertion is successful, update sentToVendor = 1 in mobile money and PegpayTranId to the pegpayId
+        }
+    }
+}
---
+++ 206/DSTV206ProcessorConsole/DSTV206ProcessorLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSTV206ProcessorLibrary
+{
+    public class Class1
+    {
+
+        // Thread 1 
+        -Picks  DSTV transactions from ReceivedTransaction table in  mobile money db where status is SUCCESS and SentToVendor is 0
+        -Calls Level 1 of bill payments to insert the transactions in ReceivedTransactions table in bill payments with status of INSERTED
+        -If Insertion Successful
+        // Thread 2
+
+    }
+}
